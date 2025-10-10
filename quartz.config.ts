28379e12ad21e70dfc9ae093c42fb0904bacc6c2
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-10 15:41:47 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -79,7 +79,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      Plugin.FolderPage(),
+      //Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
