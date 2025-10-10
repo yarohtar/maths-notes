002ea76b69fcc87b8eb9c24c6abeb3dfbbf0282c
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-10 15:21:43 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,6 +72,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest", openLinksInNewTab: true }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.HardLineBreaks(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
