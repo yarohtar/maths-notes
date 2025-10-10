8436d53725c2c38fdc6de150ddebc45f8ae99d26
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-10 16:34:37 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "yarohtar.github.io/maths-notes",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**.excalidraw"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
