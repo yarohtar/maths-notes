efbb53048486e996b81b3961ee729a7ded2a4e11
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-14 17:17:09 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -44,6 +44,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RequestReviewButton(),
   ],
 }
 
