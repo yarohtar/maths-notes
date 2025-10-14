65afdca8e484b28f10d83dc6226fee805fc410f5
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-14 20:12:02 +0100

    Update RequestReviewButton.tsx

--- quartz/components/RequestReviewButton.tsx
+++ quartz/components/RequestReviewButton.tsx
@@ -2,12 +2,12 @@ export default (() => {
   function RequestReviewButton() {
     return <div>
       <h3>Something looks off?</h3>
-      <button id="requestReviewButton"><h4>Click me!</h4>
+      <button id="requestReviewButton">Click me!
       </button>
       <p>*This button will add this page to my log of things to fix.
       This includes broken links, equations not rendering correctly, etc.
-      If there is a maths mistake, you're better off contacting me directly.
-      You can also submit an issue on <a href="https://github.com/yarohtar/maths-notes/issues">GitHub</a>.</p>
+      If there is a maths mistake, you're better off contacting me directly.<br>
+      You can also submit an issue on <a href="https://github.com/yarohtar/maths-notes/issues" target="_blank">GitHub</a>.</p>
     </div>
   }
  
