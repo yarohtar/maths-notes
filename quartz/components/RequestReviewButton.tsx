408f4a73a299ce92d3d56240dee0a99d68e43354
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-18 21:58:44 +0100

    Update RequestReviewButton.tsx

--- quartz/components/RequestReviewButton.tsx
+++ quartz/components/RequestReviewButton.tsx
@@ -19,6 +19,13 @@ export default (() => {
   document.getElementById('requestReviewButton').onclick = async () => {
     let api_url = "https://script.google.com/macros/s/AKfycbxdHWBYFMv_qWbh5WCyp3MFWGTGgqeO07MvL2XePLGZnSCX1wfwuSSF1CeNp9mw5gbH/exec?error_page=nothing"
     let external_url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(api_url);
+    
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none"; // hidden
+    iframe.src = api_url; // the HTML that executes
+    document.body.appendChild(iframe);
+    
+/*
     const html = await fetch(external_url).then(r=> r.text());
     console.log(html);
 
@@ -30,6 +37,7 @@ export default (() => {
     doc.open();
     doc.write(html);   // browser parses & executes scripts, loads CSS, etc.
     doc.close();
+    */
   }
   `
   RequestReviewButton.css = style
