3238d982d466254504e22250fbb4509b72914cf0
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-18 19:00:03 +0100

    Update RequestReviewButton.tsx

--- quartz/components/RequestReviewButton.tsx
+++ quartz/components/RequestReviewButton.tsx
@@ -15,8 +15,13 @@ export default (() => {
   }
  
   RequestReviewButton.afterDOMLoaded = `
+  
   document.getElementById('requestReviewButton').onclick = () => {
-    alert('button clicked!')
+    fetch("https://script.google.com/macros/s/AKfycbxdHWBYFMv_qWbh5WCyp3MFWGTGgqeO07MvL2XePLGZnSCX1wfwuSSF1CeNp9mw5gbH/exec?error_page=nothing", {
+    method: "GET",
+    mode: "no-cors" // Important — avoids CORS errors but you won’t see response
+    };
+    alert("button clicked!");
   }
   `
   RequestReviewButton.css = style
