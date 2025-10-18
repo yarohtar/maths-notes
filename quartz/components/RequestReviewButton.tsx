7c4b081a8ab51fb095e35adafac5e432765efeb4
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-18 20:20:13 +0100

    Update RequestReviewButton.tsx

--- quartz/components/RequestReviewButton.tsx
+++ quartz/components/RequestReviewButton.tsx
@@ -17,7 +17,9 @@ export default (() => {
   RequestReviewButton.afterDOMLoaded = `
   
   document.getElementById('requestReviewButton').onclick = async () => {
-    const html = await fetch("https://script.google.com/macros/s/AKfycbxdHWBYFMv_qWbh5WCyp3MFWGTGgqeO07MvL2XePLGZnSCX1wfwuSSF1CeNp9mw5gbH/exec?error_page=nothing").then(r=> r.text());
+    let url = "https://script.google.com/macros/s/AKfycbxdHWBYFMv_qWbh5WCyp3MFWGTGgqeO07MvL2XePLGZnSCX1wfwuSSF1CeNp9mw5gbH/exec?error_page=nothing"
+    let apilink = "https://api.allorigins.win/raw?url=" + encodeURIComponent(url);
+    const html = await fetch(apilink).then(r=> r.text());
     console.log(html);
     
     const blob = new Blob([html], { type: "text/html" });
