ccf1341bae8e93980bb03576c4389739ef900512
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-20 22:01:34 +0100

    Change TikZ block to use script tag

--- quartz/plugins/transformers/codetotikz.ts
+++ quartz/plugins/transformers/codetotikz.ts
@@ -18,7 +18,7 @@ export const MakeTikzCodeBlocks: QuartzTransformerPlugin = () => {
             {
               console.log("found tikz block");
               block.type = 'html';
-              block.value = "<source type='text/tikz'>" + block.value + "</source>";
+              block.value = "<script type='text/tikz'>" + block.value + "</script>";
               /*block = {
                 type: "text",
                 value: "<source type='text/tikz'>" + block.value + "</source>"
