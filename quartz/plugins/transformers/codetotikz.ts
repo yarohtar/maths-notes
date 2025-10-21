9305646bb5e17d21891aa70ec31577e4730d6509
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-21 11:41:42 +0100

    Disable tikzjax.js loading in codetotikz.ts
    Comment out JavaScript loading configuration for tikzjax.

--- quartz/plugins/transformers/codetotikz.ts
+++ quartz/plugins/transformers/codetotikz.ts
@@ -32,13 +32,13 @@ export const MakeTikzCodeBlocks: QuartzTransformerPlugin = () => {
               content: "https://bill-ion.github.io/tikzjax-live/dist/fonts.css",
             },
           ],
-          js: [
+          /*js: [
             {
               src: "https://bill-ion.github.io/tikzjax-live/dist/tikzjax.js",
               loadTime: "afterDOMReady",
               contentType: "external",
             },
-          ],
+          ],*/
         }
     }
   }
