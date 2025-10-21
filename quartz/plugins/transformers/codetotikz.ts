0dfb2e161a24d1c49d8cf7395f5b7c31bbfd2e1a
yarohtar <75379670+yarohtar@users.noreply.github.com>
2025-10-21 09:44:16 +0100

    Change load time event from afterDOMLoaded to afterDOMReady

--- quartz/plugins/transformers/codetotikz.ts
+++ quartz/plugins/transformers/codetotikz.ts
@@ -40,7 +40,7 @@ export const MakeTikzCodeBlocks: QuartzTransformerPlugin = () => {
           js: [
             {
               src: "https://bill-ion.github.io/tikzjax-live/dist/tikzjax.js",
-              loadTime: "afterDOMLoaded",
+              loadTime: "afterDOMReady",
               contentType: "external",
             },
           ],
